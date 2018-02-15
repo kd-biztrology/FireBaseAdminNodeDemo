f3f7155da93677dda34a3ae4e9df768bb827d475
Kevin Duan <agonyice0115@gmail.com>
2018-02-15 22:22:46 +0800

    update

---
+++ src/cloud_firestore_firebase.js
@@ -0,0 +1,4 @@
+// this is beta
+
+// different
+////   https://firebase.google.com/docs/firestore/rtdb-vs-firestore
--- src/cloud_storage.js
+++ src/cloud_storage.js
@@ -4,7 +4,23 @@ const serverAccount = require("./config/firebase-config");
 
 //firebase initializeApp
 
+//use default storage bucket
 admin.initializeApp({
     credential: admin.credential.cert(serverAccount),
-    databaseURL: "https://fir-test-8cc4a.firebaseio.com/"
+    storageBucket: "gs://fir-test-8cc4a.appspot.com"
 });
+// 'bucket' is an object defined in the @google-cloud/storage library.
+// See https://googlecloudplatform.github.io/google-cloud-node/#/docs/storage/latest/storage/bucket
+// for more details.
+//  this function must be more time to learn, is use google cloud storage
+const bucket = admin.storage.bucket();
+
+
+//custom storage bucket
+
+const customBucket = admin.storage.bucket("custom bucket path");
+
+
+//custom firebase storage bucket
+//customApp ?? i don't know this
+const  customFirebaseBucket=customApp.storage().bucket();
